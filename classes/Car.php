b92b88e228b38034c56adba17669e0ffd7d4a4a3
Edgars Jaunskalze <edgarsneko@gmail.com>
2025-04-16 21:43:05 +0300

    Acsessing data form method and proparty

--- classes/Car.php
+++ classes/Car.php
@@ -9,16 +9,28 @@ class Car
     public $vehicleType = "car";
     
     //Constructor
-
     public function __construct($brand, $color = "none") 
     {
         $this->brand = $brand;
         $this->color = $color;
     }
 
+    //Method
+    public function getCarInfo () 
+    {
+        return "Brand: ". $this->brand . ", Color: " . $this->color;
+    }
+
+
 }
 
 $car01 = new Car("Ferari", "Blue");
+//acsess method / function
+echo $car01->getCarInfo();
+echo "<br>";
+//acsess proparty
 echo $car01->vehicleType;
+
 $car02 = new Car("Volvo", "Red");
+
 $car03 = new Car("Wolfenstain", "White");
